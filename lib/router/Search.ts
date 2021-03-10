f2a29b32a44ab9e99395cc6b3954e03e0cf81c1f
lazyig <lazyig@lazyig.com>
2021-03-10 22:10:50 +0900

    fix: 레스토랑 검색 기능 코드 수정

--- lib/router/Search.ts
+++ lib/router/Search.ts
@@ -1,50 +1,21 @@
 import express from "express";
-import DB from "../DB";
+import Restaurant from "../Restaurant";
 
 export default class SearchRouter {
-    private Router: express.Router;
+  private Router: express.Router;
 
-    constructor() {
-        this.Router = express.Router();
+  constructor() {
+    this.Router = express.Router();
 
-        /* 검색 */
-        this.Router.post("/search", (req, res) => {
-            DB.getPool().getConnection((err, conn) => {
-                // DB 에러 처리
-                if(err) {
-                    console.log(err);
-                }
-                
-                conn.query(
-                  "SELECT *, COUNT(*) as review_count FROM restaurant join review WHERE restaurant_name LIKE ?",
-                  [`%${req.body.keyword}%`],
-                  (err, data) => {
-                    if (err) {
-                      console.log(err);
-                    }
+    /* 검색 */
+    this.Router.post("/search", (req, res) => {
+      Restaurant.getSearchResult(req.body.keyword).then((result) => {
+        res.json(result);
+      });
+    });
+  }
 
-                    let result = data.map((item: any) => {
-                      return {
-                        name: item.restaurant_name,
-                        score: item.restaurant_score,
-                        image: item.restaurant_image,
-                        description: item.restaurant_description,
-                        average_cooking_time: item.average_cooking_time,
-                        review_count: item.review_count
-                      };
-                    });
-
-                    // 확인용 로그
-                    console.log(data);
-                    res.json(result);
-                    conn.release();
-                  }
-                );
-            });
-        });
-    }
-
-    public getRouter(): express.Router {
-        return this.Router;
-    }
+  public getRouter(): express.Router {
+    return this.Router;
+  }
 }
