9d9fdd248c1004e75339be0714c1dbd26181972a
lazyig <kimig009@gmail.com>
2021-03-16 15:58:36 +0900

    Add: Restaurant 등록 관련 기본 추가

--- lib/Restaurant.ts
+++ lib/Restaurant.ts
@@ -48,4 +48,46 @@ export default class Restaurant {
       });
     });
   }
+  /**
+   * 
+   * @param name 식당의 이름
+   * @param address 식당의 주소
+   * @param description 식당의 설명
+   * @param categoryId 식당의 카테고리
+   */
+  static createRestuarant(name: string, address: string, description: string, categoryId: number) {
+    return new Promise((resolve, reject) => {
+      DB.getPool().getConnection((err, conn) => {
+        // DB 에러 처리
+        if (err) {
+          console.log(err);
+        }
+
+        conn.query(
+          "INSERT INTO restuarant VALUES(?, ?, ?, ?)",
+          [name, address, description, categoryId],
+          (err, data) => {
+            if (err) {
+              console.log(err);
+            }
+
+            let result = data.map((item: any) => {
+              return {
+                err: ``
+              };
+            });
+
+            // 확인용 로그
+            console.log(data);
+            conn.release();
+            resolve(result);
+          }
+        );
+      });
+    });
+  }
+
+  static uploadRestuarantImage(image: Buffer) {
+
+  }
 }
