59fafa3f3bf04dd79c862788c234d441a6734749
lazyig <kimig009@gmail.com>
2021-04-27 23:52:50 +0900

    Add: Login Route

--- lib/router/Web.ts
+++ lib/router/Web.ts
@@ -1,5 +1,17 @@
 import express from "express";
 
+let renderFunction = function(res: express.Response) {
+    return (err:Error, html: string) => {
+        if (err) {
+            console.error(err);
+            res.redirect("error.html");
+        }
+        else {
+            res.send(html);
+        }
+    }
+}
+
 export default class WebRouter {
     private Router: express.Router;
 
@@ -7,16 +19,12 @@ export default class WebRouter {
         this.Router = express.Router();
 
         this.Router.get("/", (req, res, next) => {
-            res.render("index.ejs", {}, (err, html) => {
-                if (err) {
-                    console.error(err);
-                    res.redirect("error.html");
-                }
-                else {
-                    res.send(html);
-                }
-            });
+            res.render("index.ejs", {}, renderFunction(res));
         });
+
+        this.Router.get("/login", (req, res, next) => {
+            res.render("login.ejs", {}, renderFunction(res));
+        })
     }
 
     public getRouter(): express.Router {
