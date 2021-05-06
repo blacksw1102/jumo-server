5f3482ce72f4566593699023c3b2c3cb1c7551c6
lazyig <kimig009@gmail.com>
2021-05-06 13:48:37 +0900

    Fix: FavoriteList 요구 사항 반영

--- lib/dto/FavoriteDTO.ts
+++ lib/dto/FavoriteDTO.ts
@@ -1,7 +1,21 @@
 export class FavoriteList {
-    companyNo: string;
 
-    constructor(companyNo: string) {
+    constructor(
+        companyNo: string,
+        restaurantName: string,
+        review_avg: number,
+        review_cnt: number,
+        order_cnt: number
+    ) {
         this.companyNo = companyNo
+        this.restaurantName = restaurantName
+        this.review_avg = review_avg
+        this.review_cnt = review_cnt
+        this.order_cnt = order_cnt
     }
+    companyNo: string;
+    restaurantName: string;
+    review_avg: number;
+    review_cnt: number;
+    order_cnt: number;
 }
