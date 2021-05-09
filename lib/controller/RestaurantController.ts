50262a0aff8e5a313b292a1970ceaa3537e8c71f
lazyig <kimig009@gmail.com>
2021-05-09 23:20:53 +0900

    레스토랑 정보 라우터 컨트롤러 분리

--- lib/controller/RestaurantController.ts
+++ lib/controller/RestaurantController.ts
@@ -1,1 +1,25 @@
-export default class RestaurantController {}
+import { Request, Response, NextFunction } from "express";
+import Restaurant from "../Restaurant";
+import RestaurantDAO from "../dao/RestaurantDAO";
+import jwt from "jsonwebtoken";
+import Config from "../Config";
+
+class RestaurantController {
+  public async getRestaurantInfo(req: Request, res: Response, next: NextFunction) {
+    let token: string = req.headers.authorization?.split("Bearer ")[1] || "";
+    let userId: string = "";
+
+    if (token !== "") {
+        let payload = jwt.verify(
+            token,
+            Config.getInstance().server.jwtAccessTokenExpire
+        );
+        console.debug(JSON.stringify(payload));
+    }
+
+    let result = await RestaurantDAO.getRestaurantInfo(userId, req.params.id);
+    res.status(200).json(result);
+  }
+}
+
+export default new RestaurantController();
--- lib/router/Restaurant.ts
+++ lib/router/Restaurant.ts
@@ -1,27 +1,12 @@
 import express from "express";
-import Restaurant from "../Restaurant";
-import RestaurantDAO from "../dao/RestaurantDAO";
-import jwt from "jsonwebtoken";
-import Config from "../Config";
-
+import RestaurantController from "../controller/RestaurantController";
 export default class RestaurantRouter {
   private Router: express.Router;
 
   constructor() {
     this.Router = express.Router();
 
-    this.Router.get("/:id", async (req, res, next) => {
-      let token: string = req.headers.authorization?.split('Bearer ')[1] || "";
-      let userId: string = "";
-
-      if (token !== "") {
-        let payload = jwt.verify(token, Config.getInstance().server.jwtAccessTokenExpire);
-        console.debug(JSON.stringify(payload));
-      }
-
-      let result = await RestaurantDAO.getRestaurantInfo(userId, req.params.id);
-      res.status(200).json(result);
-    });
+    this.Router.get("/:id", RestaurantController.getRestaurantInfo);
   }
 
   public getRouter(): express.Router {
