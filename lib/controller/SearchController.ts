1644164dd07014c9d6832fb440e13f077321a060
lazyig <kimig009@gmail.com>
2021-05-09 23:07:51 +0900

    #11 SearchRotuer 컨트롤러 분리

--- lib/controller/SearchController.ts
+++ lib/controller/SearchController.ts
@@ -1,1 +1,20 @@
-export default class SearchController {}
+import express, { NextFunction } from "express";
+import Restaurant from "../Restaurant";
+import RestaurantDAO from "../dao/RestaurantDAO";
+
+class SearchController {
+  public restaurantSearch(
+    req: express.Request,
+    res: express.Response,
+    next: NextFunction
+  ) {
+    const keyword: string = (req.query.keyword as string) || "";
+    const category: string = (req.query.category as string) || "";
+
+    RestaurantDAO.getSearchResult(keyword, category).then((result) => {
+      res.json(result);
+    });
+  }
+}
+
+export default new SearchController();
